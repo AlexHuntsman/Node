52f6585fd1f9309ab10f8b6a656461584667a3af
AlexHuntsman <ahun0442@740s-pl-118.local>
2016-02-29 09:07:16 -0700

    Created Calculate size method

--- src/Model/CTECList.cpp
+++ src/Model/CTECList.cpp
@@ -25,39 +25,89 @@ CTECList<Type>::~CTECList()
 template <class Type>
 Type CTECList<Type> :: removeFromFront()
 {
-	Type returnValue; //In case we need to use the value we are removing.
-
 	assert(size >0);
 	//Create a pointer to what is after head
+	Type thingToRemove;
 	ArrayNode<Type> * newHead = new ArrayNode<Type>();
 	newHead = this->head->getNext();
 	//get what was in the head node
-	returnValue = this->head->getVaue();
+	thingToRemove = this->head->getVaue();
 	//Delete what head is pointing to
 	delete this->head;
 	//Set head to the new head
 	this->head = newHead;
 
-	return returnValue;
+	this->calculateSize();
+
+	return thingToRemove;
 }
 
 template<class Type>
 Type CTECList<Type> :: removeFromIndex(int index)
 {
-	Type returnValue; // In case we need to use value we are deleting
+	Type thingToRemove; // In case we need to use value we are deleting
 
+	assert (this->size > 0);
+	assert(size > 0 && index >= 0 && index < size);
+
+	ArrayNode<Type> * previous, deleteMe, newNext;
+
+	if(index == 0)
+	{
+		thingToRemove = removeFromFront();
+	}
+	else if(index == size-1)
+	{
+		thingToRemove = removeFromEnd();
+	}
+	else
+	{
+		for(int spot = 0; spot < index+1; spot++)
+		{
+
+
+		}
+		this->calculateSize();
+	}
+
+
+	return thingToRemove;
+}
+
+template<class Type>
+void CTECList<Type> :: calculateSize()
+{
 	assert(size > 0);
-	assert(index >= 0);
-	assert(index < size);
+	assert(size >= 0);
+	ArrayNode<Type> * counterPointer = head;
+	int count = 0;
 
+	if(counterPointer == nullptr)
+	{
+		this->size = 0;
+		return;
+	}
+	else
+	{
+		count++;
+		while(counterPointer->getNext() != nullptr)
+		{
+			counterPointer = counterPointer->getNext();
+			count++;
+		}
+
+		this->count = count;
+	}
 }
 
 template<class Type>
 Type CTECList<Type> :: removeFromEnd()
 {
+
 	//Loop over size
 	//or
 	//Loop until getNext()-getNext() ==nullptr
+	//Before return the Cariable call calculateSize
 }
 
 
